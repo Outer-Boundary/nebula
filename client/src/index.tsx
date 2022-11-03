14a349be54a6f11260bea1b6eb8a633cb32487b2
Genzou <spookyjoness@outlook.com>
2022-11-03 13:25:49 +0800

    set up firestore

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,12 +5,15 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import FirebaseProvider from "./managers/FirebaseContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <FirebaseProvider>
+        <App />
+      </FirebaseProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
