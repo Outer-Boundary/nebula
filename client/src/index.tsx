33b1918ede0ac92271e323cb498bed56b33f3179
Genzou <spookyjoness@outlook.com>
2022-11-03 13:30:26 +0800

    Revert "set up firestore"
    This reverts commit 14a349be54a6f11260bea1b6eb8a633cb32487b2.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,15 +5,12 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import FirebaseProvider from "./managers/FirebaseContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <FirebaseProvider>
-        <App />
-      </FirebaseProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
