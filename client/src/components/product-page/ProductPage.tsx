13e65886981b53baaf629b720b79933f6941db36
Genzou <spookyjoness@outlook.com>
2022-12-08 13:27:56 +0800

    added basic suspense for product page (last suspense was for the main page where you can see all products)

--- client/src/components/product-page/ProductPage.tsx
+++ client/src/components/product-page/ProductPage.tsx
@@ -11,11 +11,21 @@ export default function ProductPage() {
   const location = useLocation();
   const [product, setProduct] = useState<Product | null>(location.state);
 
+  const [isLoading, setIsLoading] = useState(true);
+
   const [curImageIndex, setCurImageIndex] = useState<number>(0);
   const [curSize, setSize] = useState<Size | null>(null);
   const [curColourIndex, setColourIndex] = useState<number>(0);
 
   const { id } = useParams();
+
+  useEffect(() => {
+    console.log(curSize);
+  }, [curSize]);
+
+  useEffect(() => {
+    if (product) setIsLoading(false);
+  }, [product]);
 
   useEffect(() => {
     if (!product) {
@@ -30,45 +40,59 @@ export default function ProductPage() {
   return (
     <div className="product-page">
       <div id="image-cards-container">
-        {product?.imageUrls.map((url, index) => (
-          <img
-            className={`image-card ${curImageIndex === index && "selected"}`}
-            src={url}
-            alt=""
-            key={index}
-            onClick={() => setCurImageIndex(index)}
-          />
-        ))}
+        {isLoading
+          ? [...Array(3)].map((x, index) => <div className="image-card suspense" key={index}></div>)
+          : product?.imageUrls.map((url, index) => (
+              <img
+                className={`image-card ${curImageIndex === index && "selected"}`}
+                src={url}
+                alt=""
+                key={index}
+                onClick={() => setCurImageIndex(index)}
+              />
+            ))}
       </div>
-      <img className="product-image" src={product?.imageUrls[curImageIndex]} alt="" />
+      {isLoading ? (
+        <div className="product-image suspense"></div>
+      ) : (
+        <img className="product-image" src={product?.imageUrls[curImageIndex]} alt="" />
+      )}
       <div className="right-container">
-        <p className="name-text">{getTitleCaseStyledString(product?.title || "")}</p>
-        <p className="price-text">${product?.price}</p>
+        {isLoading ? (
+          <div className="name-text-suspense suspense"></div>
+        ) : (
+          <p className="name-text">{getTitleCaseStyledString(product?.title || "")}</p>
+        )}
+        {isLoading ? <div className="price-text-suspense suspense"></div> : <p className="price-text">${product?.price}</p>}
         <div className="divider"></div>
         <p className="colours-text">Colours</p>
         <div className="colours-container container">
-          {product?.colours.map((colour, index) => (
-            <div
-              className={`colour ${curColourIndex === index && "selected"}`}
-              style={{
-                backgroundColor: Colour[getEnumValues(Colour).find((x) => x.toLowerCase() === colour) as keyof typeof Colour] ?? "",
-              }}
-              key={index}
-              onClick={() => setColourIndex(index)}
-            ></div>
-          ))}
+          {isLoading
+            ? [...Array(3)].map((x, index) => <div className="colour suspense" key={index}></div>)
+            : product?.colours.map((colour, index) => (
+                <div
+                  className={`colour ${curColourIndex === index && "selected"}`}
+                  style={{
+                    backgroundColor: Colour[getEnumValues(Colour).find((x) => x.toLowerCase() === colour) as keyof typeof Colour] ?? "",
+                  }}
+                  key={index}
+                  onClick={() => setColourIndex(index)}
+                ></div>
+              ))}
         </div>
         <p className="sizes-text">Sizes</p>
         <div className="sizes-container container">
-          {product?.sizes.map((size, index) => (
-            <button
-              className={`size-btn ${curSize === size && "selected"}`}
-              key={index}
-              onClick={() => setSize(Size[getEnumValues(Size).find((x) => x.toLowerCase() === size) as keyof typeof Size])}
-            >
-              {size.toUpperCase()}
-            </button>
-          ))}
+          {isLoading
+            ? [...Array(3)].map((x, index) => <div className="size-btn suspense" key={index}></div>)
+            : product?.sizes.map((size, index) => (
+                <button
+                  className={`size-btn ${curSize?.toLowerCase() === size && "selected"}`}
+                  key={index}
+                  onClick={() => setSize(Size[getEnumValues(Size).find((x) => x.toLowerCase() === size) as keyof typeof Size])}
+                >
+                  {size.toUpperCase()}
+                </button>
+              ))}
         </div>
         <button className="add-to-cart-btn">Add To Cart</button>
         <details className="product-details">
