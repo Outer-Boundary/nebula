f0ffd2b08df6c7e0647b103f311b3930e5e83ae7
Genzou <spookyjoness@outlook.com>
2022-12-09 14:34:12 +0800

    changed the home/all category to include subcategories from the beauty category

--- client/src/components/types/CategoryTypes.ts
+++ client/src/components/types/CategoryTypes.ts
@@ -2,16 +2,7 @@
 const commonCategories = ["all", "new", "sale"] as const;
 
 // These categories are found in multiple category groups, like home, women, and men.
-const generalCategories = [
-  "tops",
-  "bottoms",
-  "outerwear",
-  "activewear",
-  "swimwear",
-  "underwear",
-  "shoes",
-  "accessories",
-] as const;
+const generalCategories = ["tops", "bottoms", "outerwear", "activewear", "swimwear", "underwear", "shoes", "accessories"] as const;
 
 const womenCategories = [...commonCategories, "dresses", ...generalCategories] as const;
 
@@ -19,7 +10,7 @@ const menCategories = [...commonCategories, ...generalCategories] as const;
 
 const beautyCategories = [...commonCategories, "makeup", "skincare", "equipment"] as const;
 
-const allCategories = [...new Set([...womenCategories, ...menCategories])] as const;
+const allCategories = [...new Set([...womenCategories, ...menCategories, ...beautyCategories])] as const;
 
 export const categoryCollection = {
   home: allCategories,
